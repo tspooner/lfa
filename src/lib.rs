94fd5a063e918978df94ae0448ae4ac52a850f2e
Tom Spooner <spooner10000@gmail.com>
2018-02-14 15:12:05 +0000

    feat: re-split `Approximator` into `Function` and `Parameterised`

--- src/lib.rs
+++ src/lib.rs
@@ -9,24 +9,33 @@ mod utils;
 mod error;
 pub use self::error::*;
 
-/// An interface for dealing with function approximators.
-pub trait Approximator<I: ?Sized, V> {
-
+pub trait Function<I: ?Sized, V> {
     /// Evaluates the function and returns its output.
     fn evaluate(&self, input: &I) -> EvaluationResult<V>;
+}
 
+impl<I: ?Sized, V, T> Function<I, V> for Box<T>
+where T: Function<I, V>
+{
+    fn evaluate(&self, input: &I) -> EvaluationResult<V> { (**self).evaluate(input) }
+}
+
+pub trait Parameterised<I: ?Sized, V> {
     fn update(&mut self, input: &I, update: V) -> UpdateResult<()>;
 }
 
-impl<I: ?Sized, V, T> Approximator<I, V> for Box<T>
-where T: Approximator<I, V>
+impl<I: ?Sized, V, T> Parameterised<I, V> for Box<T>
+where T: Parameterised<I, V>
 {
-    fn evaluate(&self, input: &I) -> EvaluationResult<V> { (**self).evaluate(input) }
-
     fn update(&mut self, input: &I, update: V) -> UpdateResult<()> {
         (**self).update(input, update)
     }
 }
+
+/// An interface for dealing with function approximators.
+pub trait Approximator<I: ?Sized, V>: Function<I, V> + Parameterised<I, V> {}
+
+impl<I: ?Sized, V, T> Approximator<I, V> for Box<T> where T: Approximator<I, V> {}
 
 pub mod projection;
 pub use self::projection::{Projection, Projector};
