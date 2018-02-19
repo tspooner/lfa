eac36702faadec9f2770f16f30750eacd886d348
Tom Spooner <spooner10000@gmail.com>
2018-02-19 15:10:15 +0000

    style: formatting changes to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -9,33 +9,33 @@ mod utils;
 mod error;
 pub use self::error::*;
 
+
 pub trait Function<I: ?Sized, V> {
     /// Evaluates the function and returns its output.
     fn evaluate(&self, input: &I) -> EvaluationResult<V>;
 }
 
-impl<I: ?Sized, V, T> Function<I, V> for Box<T>
-where T: Function<I, V>
-{
+impl<I: ?Sized, V, T: Function<I, V>> Function<I, V> for Box<T> {
     fn evaluate(&self, input: &I) -> EvaluationResult<V> { (**self).evaluate(input) }
 }
+
 
 pub trait Parameterised<I: ?Sized, V> {
     fn update(&mut self, input: &I, update: V) -> UpdateResult<()>;
 }
 
-impl<I: ?Sized, V, T> Parameterised<I, V> for Box<T>
-where T: Parameterised<I, V>
-{
+impl<I: ?Sized, V, T: Parameterised<I, V>> Parameterised<I, V> for Box<T> {
     fn update(&mut self, input: &I, update: V) -> UpdateResult<()> {
         (**self).update(input, update)
     }
 }
 
+
 /// An interface for dealing with function approximators.
 pub trait Approximator<I: ?Sized, V>: Function<I, V> + Parameterised<I, V> {}
 
-impl<I: ?Sized, V, T> Approximator<I, V> for Box<T> where T: Approximator<I, V> {}
+impl<I: ?Sized, V, T: Approximator<I, V>> Approximator<I, V> for Box<T> {}
+
 
 pub mod projection;
 pub use self::projection::{Projection, Projector};
