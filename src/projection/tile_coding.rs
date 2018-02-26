4eb8cdf22e3597473813820430f01ce017254917
Tom Spooner <spooner10000@gmail.com>
2018-02-26 14:54:02 +0000

    refactor(projection): extract tile coding logic into reusable, inline functions

--- src/projection/tile_coding.rs
+++ src/projection/tile_coding.rs
@@ -3,6 +3,29 @@ use geometry::{Space, Span};
 use rand::ThreadRng;
 use std::hash::{BuildHasher, Hasher};
 
+
+#[inline]
+fn bin_state(input: &[f64], n_tilings: usize) -> Vec<usize> {
+    input.into_iter().map(|f| (*f*n_tilings as f64).floor() as usize).collect()
+}
+
+
+#[inline]
+fn hash_state<H: Hasher>(mut hasher: H, state: &[usize],
+                         n_tilings: usize, memory_size: usize) -> Vec<usize> {
+    let state_len = state.len();
+
+    (0..n_tilings).map(|t| {
+        hasher.write_usize(t);
+        for i in 0..state_len {
+            hasher.write_usize((state[i] + t + i*t*2)/n_tilings)
+        }
+
+        hasher.finish() as usize % memory_size
+    }).collect()
+}
+
+
 /// Generalised tile coding scheme with hashing.
 #[derive(Clone, Serialize, Deserialize)]
 pub struct TileCoding<H: BuildHasher> {
@@ -33,25 +56,9 @@ impl<H: BuildHasher> Space for TileCoding<H> {
 
 impl<H: BuildHasher> Projector<[f64]> for TileCoding<H> {
     fn project(&self, input: &[f64]) -> Projection {
-        let mut hasher = self.hasher_builder.build_hasher();
+        let state = bin_state(input, self.n_tilings);
+        let hasher = self.hasher_builder.build_hasher();
 
-        let n_floats = input.len();
-        let floats: Vec<usize> = input
-            .iter()
-            .map(|f| (*f * self.n_tilings as f64).floor() as usize)
-            .collect();
-
-        Projection::Sparse(
-            (0..self.n_tilings)
-                .map(|t| {
-                    hasher.write_usize(t);
-                    for i in 0..n_floats {
-                        hasher.write_usize((floats[i] + t + i * t * 2) / self.n_tilings)
-                    }
-
-                    hasher.finish() as usize % self.memory_size
-                })
-                .collect(),
-        )
+        hash_state(hasher, &state, self.n_tilings, self.memory_size).into()
     }
 }
