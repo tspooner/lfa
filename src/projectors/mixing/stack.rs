08f8b332a90a385b93129f471c222bdb4e95bc32
Tom Spooner <spooner10000@gmail.com>
2018-05-04 14:38:13 +0100

    refactor(projections): extract stacking logic to static function

--- src/projectors/mixing/stack.rs
+++ src/projectors/mixing/stack.rs
@@ -3,6 +3,24 @@ use projectors::{Projector, Projection, DenseT};
 use rand::ThreadRng;
 use std::marker::PhantomData;
 
+fn stack_projections(p1: Projection, n1: usize, p2: Projection, n2: usize) -> Projection {
+    use Projection::*;
+
+    match (p1, p2) {
+        (Sparse(mut p1_indices), Sparse(p2_indices)) => {
+            p2_indices.iter().for_each(|&i| { p1_indices.insert(i+n1); });
+
+            Sparse(p1_indices)
+        },
+        (p1, p2) => {
+            let mut all_activations = p1.expanded(n1).to_vec();
+            all_activations.extend_from_slice(p2.expanded(n2).as_slice().unwrap());
+
+            Dense(DenseT::from_vec(all_activations))
+        },
+    }
+}
+
 pub struct Stack<I: ?Sized, P1: Projector<I>, P2: Projector<I>> {
     p1: P1,
     p2: P2,
@@ -19,13 +37,16 @@ impl<I: ?Sized, P1: Projector<I>, P2: Projector<I>> Stack<I, P1, P2> {
             phantom: PhantomData,
         }
     }
+
 }
 
 impl<I: ?Sized, P1: Projector<I>, P2: Projector<I>> Space for Stack<I, P1, P2> {
     type Value = Projection;
 
-    fn sample(&self, _: &mut ThreadRng) -> Projection {
-        unimplemented!()
+    fn sample(&self, rng: &mut ThreadRng) -> Projection {
+        let (n1, n2) = (self.p1.dim(), self.p2.dim());
+
+        stack_projections(self.p1.sample(rng), n1, self.p2.sample(rng), n2)
     }
 
     fn dim(&self) -> usize {
@@ -39,29 +60,9 @@ impl<I: ?Sized, P1: Projector<I>, P2: Projector<I>> Space for Stack<I, P1, P2> {
 
 impl<I: ?Sized, P1: Projector<I>, P2: Projector<I>> Projector<I> for Stack<I, P1, P2> {
     fn project(&self, input: &I) -> Projection {
-        use Projection::*;
+        let (n1, n2) = (self.p1.dim(), self.p2.dim());
 
-        let p1 = self.p1.project(input);
-        let p2 = self.p2.project(input);
-
-        match (p1, p2) {
-            (Sparse(mut p1_indices), Sparse(p2_indices)) => {
-                let offset: usize = self.p1.dim();
-
-                p2_indices.iter().for_each(|&i| { p1_indices.insert(i+offset); });
-
-                Sparse(p1_indices)
-            },
-            (p1, p2) => {
-                let p1_activations = p1.expanded(self.p1.dim());
-                let p2_activations = p2.expanded(self.p2.dim());
-
-                let mut all_activations = p1_activations.to_vec();
-                all_activations.extend_from_slice(p2_activations.as_slice().unwrap());
-
-                Dense(DenseT::from_vec(all_activations))
-            },
-        }
+        stack_projections(self.p1.project(input), n1, self.p2.project(input), n2)
     }
 }
 
