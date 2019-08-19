f8a0dd4ea8d4063ee459819c5fd5f2dc5d9e10e8
Tom Spooner <spooner10000@gmail.com>
2019-08-19 12:43:08 +0100

    test: update linear_regression test after changes

--- lfa/tests/linear_regression.rs
+++ lfa/tests/linear_regression.rs
@@ -1,12 +1,12 @@
 extern crate lfa;
 extern crate rand;
+extern crate rand_distr;
 
-use rand::{distributions::Uniform, Rng, thread_rng};
+use rand::{Rng, thread_rng};
+use rand_distr::Uniform;
 use self::lfa::{
-    basis::fixed::Polynomial,
-    composition::Composable,
-    core::{Parameterised, Approximator, Embedding},
-    LFA,
+    Approximator, Parameterised, ScalarFunction, Features,
+    basis::{Projector, Polynomial},
 };
 
 #[test]
@@ -14,39 +14,16 @@ fn scalar() {
     const M: f64 = 0.1;
     const C: f64 = -0.05;
 
-    let mut fa = LFA::scalar(Polynomial::new(1, vec![(-1.0, 1.0)]).with_constant());
-    let mut rng = thread_rng();
+    let basis = Polynomial::new(1, 1).with_constant();
+    let mut fa = ScalarFunction::zeros(basis.n_features());
 
-    for x in rng.sample_iter(&Uniform::new_inclusive(-1.0, 1.0)).take(1000) {
+    for x in thread_rng().sample_iter(&Uniform::new_inclusive(-1.0, 1.0)).take(1000) {
         let y_exp = M*x + C;
 
-        let x = fa.embed(&vec![x]);
+        let x = basis.project(&vec![x]);
         let y_apx = fa.evaluate(&x).unwrap();
 
         fa.update(&x, (y_exp - y_apx) * 0.1).ok();
-    }
-
-    let weights = fa.weights();
-    let weights = weights.column(0);
-
-    assert!(weights.all_close(&vec![M, C].into(), 1e-3))
-}
-
-#[test]
-fn scalar_manual() {
-    const M: f64 = 0.1;
-    const C: f64 = -0.05;
-
-    let mut fa = LFA::scalar(Polynomial::new(1, vec![(-1.0, 1.0)]).with_constant());
-    let mut rng = thread_rng();
-
-    for x in rng.sample_iter(&Uniform::new_inclusive(-1.0, 1.0)).take(1000) {
-        let y_exp = M*x + C;
-
-        let x = fa.embed(&vec![x]);
-        let y_apx = fa.evaluate(&x).unwrap();
-
-        fa.weights_view_mut().column_mut(0).scaled_add((y_exp - y_apx) * 0.1, &x.expanded(2));
     }
 
     let weights = fa.weights();
